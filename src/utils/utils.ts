0a4273ced1c6a037cb51495c4911d2fd3dd0356c
kostili-tec <weh4teyou@gmail.com>
2023-11-17 23:06:54 +0300

    fix: password regex

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,8 @@
 import { TypeUser } from '@/types/types';
 
 const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&#])[\w@$!%*?&#]{8,}$/;
+// минимум 8 символов. включая большие, маленькие буквы, цифры и символы  @, $, !, %, *, ? и #.
 
 export const isValidUser = ({ email, password }: TypeUser) =>
   emailRegex.test(email) && passwordRegex.test(password);
